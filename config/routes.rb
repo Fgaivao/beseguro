7c212d4c6ee32f812cdce54694b90b1d4f5c9528
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2019-04-30 16:29:48 +0100

    setup contact form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :admins
   get 'seguros/search' => 'seguros#search', as: 'search_seguros'
   resources :seguros
+  resources :contacts, only: [:new, :create]
   resources :teams, :path => "contactos"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
