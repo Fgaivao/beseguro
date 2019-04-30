83faeea37ee9570ca90025de5be1c6ce3ee8edcb
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2019-04-30 16:48:44 +0100

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {host: 'http://beseguro.herokuapp.com' }
-  config.action_mail.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
