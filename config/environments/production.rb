1832f4b847ba6f852c998c968fcdefbfa00082d1
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2019-04-30 16:43:52 +0100

    sg and sq

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,14 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: 'http://beseguro.herokuapp.com' }
   config.action_mail.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
