ff3cfc0156e45c8262c5e4810ef34f5bc9dadf1d
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2019-04-30 16:40:45 +0100

    sgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {host: 'http://beseguro.herokuapp.com' }
+  config.action_mail.delivery_method = :smtp
 end
