bf39093dc8ddb929369da3eb8f5ba730706c2f7b
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2019-05-26 15:19:10 +0100

    segurocontact

--- app/controllers/seguros_controller.rb
+++ app/controllers/seguros_controller.rb
@@ -71,6 +71,21 @@ class SegurosController < ApplicationController
       def solucoes
     end
 
+    def segurocontactnew
+    @segurocontact = SeguroContact.new
+  end
+
+  def segurocontactcreate
+    @segurocontact = SeguroContact.new(params[:contact])
+    @segurocontact.request = request
+    if @segurocontact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = "Erro"
+      render :new
+    end
+  end
+
 
 
 
--- app/models/segurocontact.rb
+++ app/models/segurocontact.rb
@@ -1,9 +1,11 @@
-class Contact < MailForm::Base
+class SeguroContact < MailForm::Base
+  belongs_to :seguro
 
   attribute :name, :validate => true
   attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message, :valitade => true
   attribute :nickname, :captcha => true
+  attribute :seguro, :validate => false
 
   def headers
     {
@@ -14,5 +16,3 @@ class Contact < MailForm::Base
 
 end
 end
-
-
