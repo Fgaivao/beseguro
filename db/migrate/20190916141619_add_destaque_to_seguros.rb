8429acc66b0659703ae6488093f9c211fbda9e1f
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2019-09-16 16:01:22 +0100

    des

--- db/migrate/20190916141619_add_destaque_to_seguros.rb
+++ db/migrate/20190916141619_add_destaque_to_seguros.rb
@@ -1,4 +1,4 @@
-class AddDestequeToSeguros < ActiveRecord::Migration[5.2]
+class AddDestaqueToSeguros < ActiveRecord::Migration[5.2]
   def change
     add_column :seguros, :destaque, :boolean
   end
