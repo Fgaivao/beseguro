bebc1096b347e780f660355ff23fb2df4bf33665
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2019-09-16 09:58:59 +0100

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_151706) do
+ActiveRecord::Schema.define(version: 2019_09_16_080809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2018_09_02_151706) do
     t.string "tipo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_seguros_on_slug", unique: true
   end
 
   create_table "teams", force: :cascade do |t|
